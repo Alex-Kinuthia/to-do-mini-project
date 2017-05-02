fdaf6d9110a8349c8200bf201dc023af4649ac3e
Alex-Kinuthia <alexkinuthia125@gmail.com>
2017-05-02 14:17:39 +0300

    modified:   build.gradle  modified:   src/main/java/App.java  modified:   src/main/java/Category.java  new file:   src/main/java/DB.java  modified:   src/main/java/Task.java  modified:   src/test/java/CategoryTest.java  modified:   src/test/java/TaskTest.java

--- src/main/java/Category.java
+++ src/main/java/Category.java
@@ -1,56 +1,68 @@
 import java.util.List;
 import java.util.ArrayList;
+import org.sql2o.*;
 
-// access modifiers
 public class Category {
-  private String mName;
-  private static List<Category> instances = new ArrayList<Category>();
-  private int mId;
-  private List<Task> mTasks;
+  private String name;
+  private int id;
 
-// holding the instances of the class Category.java
   public Category(String name) {
-    mName = name;
-    instances.add(this);
-    mId = instances.size();
-    mTasks = new ArrayList<Task>();
+    this.name = name;
   }
 
-// to display categories the user will select
   public String getName() {
-    return mName;
+    return name;
   }
-
 
   public static List<Category> all() {
-    return instances;
-  }
-
-  public static void clear() {
-    instances.clear();
+    String sql = "SELECT id, name FROM categories";
+    try(Connection con = DB.sql2o.open()) {
+      return con.createQuery(sql).executeAndFetch(Category.class);
+    }
   }
 
   public int getId() {
-    return mId;
+    return id;
   }
 
-  // public static Category find(int id) {
-  //   return instances.get(id - 1);
-  // }
+ public static Category find(int id) {
+     try(Connection con = DB.sql2o.open()) {
+       String sql = "SELECT * FROM categories where id=:id";
+       Category category = con.createQuery(sql)
+         .addParameter("id", id)
+         .executeAndFetchFirst(Category.class);
+       return category;
+     }
+   }
 
-  public List<Task> getTasks() {
-    return mTasks;
-  }
+ public List<Task> getTasks() {
+   try(Connection con = DB.sql2o.open()) {
+     String sql = "SELECT * FROM tasks where categoryId=:id";
+     return con.createQuery(sql)
+       .addParameter("id", this.id)
+       .executeAndFetch(Task.class);
+   }
+ }
 
-  public void addTask(Task task) {
-    mTasks.add(task);
-  }
+ @Override
+ public boolean equals(Object otherCategory) {
+   if (!(otherCategory instanceof Category)) {
+     return false;
+   } else {
+     Category newCategory = (Category) otherCategory;
+     return this.getName().equals(newCategory.getName()) &&
+            this.getId() == newCategory.getId();
+   }
+ }
 
-  public static Category find(int id) {
-    try {
-      return instances.get(id -1);
-    } catch (IndexOutOfBoundsException exception) {
-      return null;
+  public void save() {
+    try(Connection con = DB.sql2o.open()) {
+      String sql = "INSERT INTO categories(name) VALUES (:name)";
+      this.id = (int) con.createQuery(sql, true)
+        .addParameter("name", this.name)
+        .executeUpdate()
+        .getKey();
     }
   }
+
 }
